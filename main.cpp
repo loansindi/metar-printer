4c7751ffed442cd1091d54ba58232878964a89a4
Derek Bever <derek.bever@gmail.com>
2018-05-17 14:42:49 -0500

    shuffle pi stuff

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "buttonconfig.h"
 
 
-void button(void) {w.printReport("KORD");}
+
 
 int main(int argc, char *argv[])
 {
@@ -17,8 +17,6 @@ int main(int argc, char *argv[])
     wiringPiSetup();
     pinMode(9, INPUT);
     pullUpDnControl(9, PUD_UP);
-    wiringPiISR(9, INT_EDGE_FALLING, &button);
-
 
     return a.exec();
 

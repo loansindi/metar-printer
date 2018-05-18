b9059a23715377273078659c561b77be620938ac
Derek Bever <derek.bever@gmail.com>
2018-05-18 10:53:25 -0500

    .

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
     w.show();
 
 
-    wiringPiISR(9, INT_EDGE_FALLING, w.button);
+    wiringPiISR(9, INT_EDGE_FALLING, button);
     wiringPiSetup();
     pinMode(9, INPUT);
     pullUpDnControl(9, PUD_UP);
